dccface066d6e7b83d8be9b6d0707bd214abc639
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-11-29 12:13:16 +0530

    Add project files.

---
+++ learning/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//UNIT 1
+// introduction about c# programming
+
+
+//UNIT 2
+//writing first line of code
+Console.WriteLine("Hello World!");
+
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
+
+Console.WriteLine();
+//UNIT 3
+// understanding compilation and syntax in programming.
+
+
+//UNIT 4
+// code challenge to use two different techniques to print the two lines of the output.
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+
+
+//UNIT 5
+//steps to display code in 2 different lines
+
+
+//UNIT 6
+//Knowledge checking
+//solving 3 mcqs
+
+
+//UNIT 7
+//summary and furthur learning
+
+
+
+
